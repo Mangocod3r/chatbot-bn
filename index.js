30b389fa60afc035e1fb0a8b80de3557efde43d3
Khiran Gannu <21bd1a056ncsed@gmail.com>
2023-11-30 06:28:09 +0530

    Updated server file

--- index.js
+++ index.js
@@ -5,37 +5,72 @@ const PORT = 4000;
 const http = require('http').Server(app);
 const cors = require('cors');
 
-
 const socketIO = require('socket.io') (http, {
-    cors: {
-        origin: ["http://localhost:3000", "http://192.168.1.3:3000"],    }
+    cors: { origin: "*",}
 });
 
-app.use(cors());
+// app.use(cors());
+
+app.use(cors({
+    origin: '*', // Replace with the correct port
+    credentials: true, // You may need this option if you're sending cookies or sessions
+  }));
 let users = [];
+let rooms = {};
 
 socketIO.on('connection', (socket) => {
     console.log(`${socket.id} user just connected`);
 
     socket.on('message', (data) => {
         // console.log(data);
-        socketIO.emit('messageResponse', data);
+        const room = data.room;
+        const message = { name: data.name, text: data.text, timestamp: data.timestamp };
+
+        if (!rooms[room]) {
+            rooms[room] = [];
+        }
+
+        rooms[room].push(message);
+
+        socketIO.to(room).emit('messageResponse', message);
+        // socketIO.emit('messageResponse', data);
     });
 
-    socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+    // socket.on('typing', (data) => socket.broadcast.emit('typingResponse', data));
+    socket.on('typing', (data) => socket.broadcast.to(data.room).emit('typingResponse', data));
 
-    socket.on('resetTypingStatus', (data) => socket.broadcast.emit('typingResponse', data));
+    socket.on('resetTypingStatus', (data) => socket.broadcast.to(data.room).emit('typingResponse', data));
 
+    // socket.on('resetTypingStatus', (data) => socket.broadcast.emit('typingResponse', data));
+
+    // socket.on('newUser', (data) => {
+    //     users.push(data);
+    //     socketIO.emit('newUserResponse', users);
+    // });
     socket.on('newUser', (data) => {
-        users.push(data);
-        socketIO.emit('newUserResponse', users);
+        users[socket.id] = { userName: data.userName, socketID: socket.id };
+        socketIO.emit('newUserResponse', Object.values(users));
+    });
+
+    socket.on('joinRoom', (data, messages) => {
+        const room = data.room;
+        socket.join(room);
+        // console.log(messages);
+        // const message = { name: messages.id, text: messages.text, timestamp: messages.timestamp };
+
+        // if (rooms[room]) {
+        //     socketIO.to(room).emit('pastMessages', data);
+        // }
+
+        socketIO.to(room).emit('newUserResponse', Object.values(users));
     });
 
     socket.on('disconnect', () => {
         console.log('A user disconnected');
-        users = users.filter((user) => user.socketID !== socket.id);
+        // delete users[socket.id];
+        // users = users.filter((user) => user.socketID !== socket.id);
 
-        socketIO.emit('newUserResponse', users);
+        socketIO.emit('newUserResponse', Object.values(users));
         socket.disconnect();
     });
 });
